1240e1cce6f98de5e0fcb0a65e75ca0e7b8336c7
wjzheng-zzz <1041404542@qq.com>
2024-03-27 17:27:20 +0800

    create post Activity

---
+++ GroupProject/GroupProject/app/src/main/java/com/example/groupproject/PostCreateActivity.java
@@ -0,0 +1,13 @@
+package com.example.groupproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PostCreateActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
