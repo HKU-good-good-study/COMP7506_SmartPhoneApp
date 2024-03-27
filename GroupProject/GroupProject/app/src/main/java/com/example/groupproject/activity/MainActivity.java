82d545bb28e35a12259815d5b2fa318670583f63
Yutong13 <yutong13@ualberta.ca>
2024-03-27 17:17:15 +0800

    Add file strucutre

--- GroupProject/GroupProject/app/src/main/java/com/example/groupproject/activity/MainActivity.java
+++ GroupProject/GroupProject/app/src/main/java/com/example/groupproject/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.groupproject;
+package com.example.groupproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.groupproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
