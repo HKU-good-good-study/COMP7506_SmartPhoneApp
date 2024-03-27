13bf0865fbc87809a7c0afd19a234cd842515f15
Yutong13 <yutong13@ualberta.ca>
2024-03-27 19:45:52 +0800

    Implement firestore dependencies gradle

--- GroupProject/GroupProject/app/build.gradle.kts
+++ GroupProject/GroupProject/app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 }
