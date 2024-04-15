c152e4b8430ef033dc166b2e06195cb814cd2af4
zihansu5 <zsu277@gmail.com>
2024-04-16 02:49:30 +0800

    refined leaderboard layout; add func of posting in leaderboard

--- GroupProject/app/src/main/java/com/example/groupproject/activity/LeaderboardActivity.java
+++ GroupProject/app/src/main/java/com/example/groupproject/activity/LeaderboardActivity.java
@@ -1,8 +1,16 @@
 package com.example.groupproject.activity;
 
+import static android.icu.lang.UProperty.INT_START;
+
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.LeadingMarginSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -33,6 +41,7 @@ public class LeaderboardActivity extends AppCompatActivity {
     TextView titlerank;
     String current_username;
     int current_ranking;
+    int current_post_count;
 
     DatabaseCallback getDbcallbackuser =  new DatabaseCallback(this) {
         @Override
@@ -50,8 +59,6 @@ public class LeaderboardActivity extends AppCompatActivity {
         public void run(List<Object> dataList) {
             dbcontroller.getCurrentUser(getDbcallbackuser, Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID));
 
-            System.out.println("!!!!!!!!!!!!! current user**************");
-            System.out.println(current_username);
 
             ArrayList<ArrayList<Object>> display_list = new ArrayList<>();
 
@@ -64,13 +71,12 @@ public class LeaderboardActivity extends AppCompatActivity {
                 ArrayList<Object> display_item = new ArrayList<>();
                 display_item.add(username);
                 display_item.add(count);
-                
+
                 display_list.add(display_item);
             }
-            
+
 //            display_list.sort(Comparator.comparing(list -> (Integer) list.get(1)));
             display_list.sort(Comparator.comparing(list -> (Integer) list.get(1), Comparator.reverseOrder()));
-
 
             current_ranking = findIndex(display_list, current_username);
             System.out.println("~~~~~~~~~~ current ranking **************");
@@ -83,16 +89,43 @@ public class LeaderboardActivity extends AppCompatActivity {
 
             titlerank = findViewById(R.id.currentranking);
             String str_currentranking = String.valueOf(current_ranking + 1);
-            String header;
 
-            if (current_ranking == -1) {
-                header = "You have not posted anything. Start sharing moments of your life now!";
+            if (current_ranking != -1) {
+                current_post_count = (int) display_list.get(current_ranking).get(1);
             }
-            else {
-                header = "Congrats! You current rank is <" + str_currentranking + ">";
+
+            if (current_ranking != -1 && current_post_count == 0) {
+                String text = "You have not posted anything.\nClick here to share moments of your life now!";
+                SpannableStringBuilder header = new SpannableStringBuilder(text);
+                int start = 0;
+                int end = text.length();
+                int start2 = text.indexOf("Click");
+                header.setSpan(new android.text.style.StyleSpan(Typeface.BOLD), start2, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                header.setSpan(new ForegroundColorSpan(Color.parseColor("#E8B4D0")), start2, end, 0);
+                header.setSpan(new android.text.style.RelativeSizeSpan(1.2f), start2, end, 0);
+                titlerank.setPadding(20, 20, 20, 0);
+                titlerank.setText(header);
             }
-            titlerank.setText(header);
+            else if (current_ranking != -1) {
+//                header = "Congrats! You current rank is <" + str_currentranking + ">";
+                String text = "Congrats! You current rank is   " + str_currentranking;
+                SpannableStringBuilder header = new SpannableStringBuilder(text);
+                int startrank = text.indexOf("is") + 5;
+                int endrank = startrank + str_currentranking.length();
+                int starttext = 0;
+                int endtext = starttext + "Congrats! You current rank is   ".length();
+                // format rank
+                header.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), startrank, endrank, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                header.setSpan(new android.text.style.RelativeSizeSpan(2.8f), startrank, endrank, 0);
+                header.setSpan(new ForegroundColorSpan(Color.parseColor("#E8B4D0")), startrank, endrank,0);
+                // format text
+                header.setSpan(new android.text.style.StyleSpan(Typeface.ITALIC), starttext, endtext, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                titlerank.setPadding(80,50, 20, 0);
+                titlerank.setText(header);
+                titlerank.setText(header);
+            }
         }
+
         @Override
         public void successlistener(Boolean success) {
 
@@ -112,6 +145,16 @@ public class LeaderboardActivity extends AppCompatActivity {
 //                startActivity(new Intent(LeaderboardActivity.this, PostCreateActivity.class));
             }
         });
+
+        if (current_post_count == 0) {
+            TextView postbtn = findViewById(R.id.currentranking);
+            postbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(LeaderboardActivity.this, MapActivity.class));
+                }
+            });
+        }
 
     }
     public static int findIndex(List<ArrayList<Object>> display_list, String current_username) {
